a52eb90c213925516ece62e5c12bf7dc55ef10fb
Felipe Barreti <harnbb@gmail.com>
2015-09-25 22:47:04 -0300

    push master

---
+++ bower.js
@@ -0,0 +1,18 @@
+{
+  "name": "widgets-spa",
+  "version": "0.0.0",
+  "homepage": "https://github.com/RedVentures/widgets-spa",
+  "license": "MIT",
+  "ignore": [
+    "**/.*",
+    "node_modules",
+    "bower_components",
+    "test",
+    "tests"
+  ],
+  "dependencies": {
+    "angular": "1.4.3",
+    "angular-route": "1.4.3",
+    "underscore": "1.8.3"
+  }
+}
